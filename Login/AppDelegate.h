89b9828595c71cb3eb512db13594b0c65f558fa2
herman <herman@hermandeMac-mini.local>
2015-06-15 17:03:20 +0800

    Initial Commit

---
+++ Login/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Login
+//
+//  Created by herman on 15/6/15.
+//  Copyright (c) 2015年 Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Login/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Login
+//
+//  Created by herman on 15/6/15.
+//  Copyright (c) 2015年 Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
